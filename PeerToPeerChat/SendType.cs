60a40b4fd342d77afaff08a79204cd12353acd45
thuyduong991234 <thuyduong991234@gmail.com>
2018-12-18 15:29:53 +0700

    Fix bug ChatForm

---
+++ PeerToPeerChat/SendType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerToPeerChat
+{
+    public enum SendType
+    {
+        MESSAGE,
+        FILE,
+        IMAGE,
+        NONE
+    }
+}
